681ec24f1d876d965cfd39703ce01d23b6fbf6db
divyamth <divyamtembhare23@gmail.com>
2024-08-26 18:35:15 +0530

    add extra components for the landing page

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import Link from "next/link";
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 import { usePathname } from "next/navigation";
+import Footer from "./Footer";
 
 const MobileNav = ({ user }: MobileNavProps) => {
     const pathname = usePathname();
@@ -36,7 +37,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                         alt="Horizon"
                     />
                     <h1 className="text-26 font-ibm-plex-serif font-bold">Horizon</h1>
-                </Link>
+        </Link>
 
                 <div className="mobilenav-sheet">
                     <SheetClose asChild>
@@ -66,6 +67,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                     })}
                         </nav>
                     </SheetClose>
+                    <Footer user={user} type="mobile" />
                 </div>
         </SheetContent>
       </Sheet>
